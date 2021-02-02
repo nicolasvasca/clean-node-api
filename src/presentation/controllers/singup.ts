b5db638fac54945298eb1833b9d67f7e22c1367b
Nicolas Vasca <nicolas.vasca@usemobile.xyz>
2021-02-02 16:35:21 -0300

    feat:  ebsyure SingUpCOntroller returns 400 if password confimation fails

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -23,6 +23,9 @@ export class SingUpController implements Controller {
           return badRequest(new MissingParamError(field))
         }
       }
+      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
       const isvalid = this.emailValidator.isValid(httpRequest.body.email)
       if (!isvalid) {
         return badRequest(new InvalidParamError('email'))
