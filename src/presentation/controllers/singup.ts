a558b1a2415573126852815cfb27ddcc5813df14
Nicolas Vasca <nicolas.vasca@usemobile.xyz>
2021-02-02 15:07:42 -0300

    feat: ensure SingUpController return 400 if invalid emial is provided

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -1,9 +1,17 @@
 import { HttpResponse, HttpRequest } from '../protocols/http'
 import { MissingParamError } from '../error/missing-param-error'
+import { InvalidParamError } from '../error/invalid-param-error'
 import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
+import { EmailValidator } from '../protocols/email-validator'
 
 export class SingUpController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     /* if (!httpRequest.body.name) {
       return badRequest(new MissingParamError('name'))
@@ -16,6 +24,10 @@ export class SingUpController implements Controller {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
+    }
+    const isvalid = this.emailValidator.isValid(httpRequest.body.email)
+    if (!isvalid) {
+      return badRequest(new InvalidParamError('email'))
     }
   }
 }
