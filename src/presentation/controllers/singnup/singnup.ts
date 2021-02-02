c3fb9d66487dee4c09c2c8574a7c3a1702dc2642
Nicolas Vasca <nicolas.vasca@usemobile.xyz>
2021-02-02 17:34:49 -0300

    feat: ensure SingUpController returns 200

--- src/presentation/controllers/singnup/singnup.ts
+++ src/presentation/controllers/singnup/singnup.ts
@@ -33,11 +33,15 @@ export class SingUpController implements Controller {
       if (!isvalid) {
         return badRequest(new InvalidParamError('email'))
       }
-      this.addAccount.add({
+      const account = this.addAccount.add({
         name,
         email,
         password
       })
+      return {
+        statusCode: 200,
+        body: account
+      }
     } catch (error) {
       return serverError()
     }
